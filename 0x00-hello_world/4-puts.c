d5c44422e4006a25aa877d73afdeb8488812184c
Eugo22 <eugenecodez3@gmail.com>
2023-07-13 14:04:17 -0700

    4-puths.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main -  entry poit
- *return:  always 0 (success)
+ * main - entry poit
+ * return: always 0 (success)
  */
 int main(void)
 {
-	paths("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
