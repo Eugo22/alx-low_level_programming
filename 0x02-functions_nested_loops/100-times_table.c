0b5142d7b65198dab73bfcb3d4fab42316d63e4b
Eugo22 <eugenecodez3@gmail.com>
2023-07-19 08:30:41 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_times_table - print the n times table, starting with 0
+ * @n: The value of the tomes table to be printed.
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n < 0 || n > 15)
+		return;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			int k = j * i;
+
+			if (j == 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (k < 10)
+			{
+				putchar(' ');
+				_putchar(' ');
+			}
+			else if (k < 100)
+			{
+				_putchar(' ');
+			}
+			_putchar((k / 100) + '0');
+			_putchar(((k / 10) % 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
