8fb527ce91411dd06db5246bcc71b1c6960a0bda
Eugo22 <eugenecodez3@gmail.com>
2023-07-18 17:45:09 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,8 @@ void times_table(void)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
-				_putchar(44);
-				_putchar(32);
+				_putchar(',');
+				_putchar(' ');
 				_putchar(d + '0');
 				_putchar(u + '0');
 			}
@@ -25,13 +25,13 @@ void times_table(void)
 			{
 				if (y != 0)
 				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 				_putchar(z + '0');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
