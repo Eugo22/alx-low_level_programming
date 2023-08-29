70fb2139076e958e84a21bd7fae1efba4626739a
Eugo22 <eugenecodez@gmail.com>
2023-08-29 15:25:45 -0700

    finaaly

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,7 +12,7 @@ size_t listint_len(const listint_t *h)
 	size_t node = 0;
 	const listint_t *current = h;
 
-	while (current == NULL)
+	while (current != NULL)
 	{
 		node++;
 		current = current->next;
